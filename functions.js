ed4d0c1334e0b1b54d246613817a24daed1a51be
Doreen Wu <76doreen@gmail.com>
2016-08-11 11:36:32 -0400

    This is my Functions page.

---
+++ functions.js
@@ -0,0 +1,11 @@
+function Function1() {
+	alert('Warning! You just got a virus!');
+}
+
+function Function2() {
+	alert('Malware is taking over your device!')
+}
+
+function Function4() {
+	alert('Your device is now completely infected!')
+}
